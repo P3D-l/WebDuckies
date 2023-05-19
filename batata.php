0a61f9b60b5ab8f3e51ac6dfb9805b86807992fd
Pedro Lucas <plucasn@yahoo.com>
2023-05-18 22:35:31 -0300

    first commit

---
+++ batata.php
@@ -0,0 +1,3 @@
+<?php
+    echo "Lets go babies, have a nice and cozy hatch, your parents love ya <br\n";
+?>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+    <h1>Letsa go babies, have a nice and cozy hatch, your parents love ya</h1>
+    <p>(Don't make ya mommy worry like that never again tho, or no videogames for a month)</p>
+
+    <img src="ducklings.jpg">
+
+</body>
+
+</html>
