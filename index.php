b8a8d0daff6f69a62ffe259ca129cc426e2cd0ac
Pedro Lucas <plucasn@yahoo.com>
2023-05-19 12:58:57 -0300

    Mr Thing hatched

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 
 
     <h1>Letsa go babies, have a nice and cozy hatch, Keep pippin you are doing great <3</h1>
+    <p>Congrats Mr thing!!!! You did it! Mommy and daddy are soooo prooooudd of ya xD</p>
     <p>(Don't make ya mommy worry like that never again tho, or no videogames for a month)</p>
 
     <img src="ducklings.jpg">
