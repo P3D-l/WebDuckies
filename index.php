bce73f8e8c095192f2a1fdb9b25bd083de4022c7
Pedro Lucas <plucasn@yahoo.com>
2023-05-20 19:27:16 -0300

    added non functional buttons

--- index.php
+++ index.php
@@ -6,11 +6,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<style>
+    .button {
+  background-color: #feffa3;
+  border: none;
+  color: #ccb8d6;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+
+.button1 {border-radius: 12px;}
+.button1:hover{background-color: #f7f8a3;}
+
+.button2 {border-radius: 12px;}
+.button2:hover{background-color: #f7f8a3;}
+</style>
+<button class = "button button1">Meet the Duckies</button> 
+<button class = "button button2">Drawings</button>
+
 <body>
 
 
     <h1>Letsa go babies, have a nice and cozy hatch, Keep pippin you are doing great <3</h1>
-    <p>Congrats Mr thing!!!! You did it! Mommy and daddy are soooo prooooudd of ya xD</p>
+    <p>Congrats Mr thing, Maxi and Goose!!!! You guys did it! Mommy and daddy are soooo prooooudd of ya xD</p>
     <p>(Don't make ya mommy worry like that never again tho, or no videogames for a month)</p>
 
     <img src="ducklings.jpg">
