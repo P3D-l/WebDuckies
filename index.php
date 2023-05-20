48aa2e0bf8359071e516c1b923a984f1b99b92ab
Pedro Lucas <plucasn@yahoo.com>
2023-05-20 20:35:29 -0300

    centralized

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     .button {
   background-color: #feffa3;
   border: none;
-  color: #ccb8d6;
+  color: #b45587;
   padding: 15px 32px;
   text-align: center;
   text-decoration: none;
@@ -20,21 +20,24 @@
   cursor: pointer;
 }
 
-.button1 {border-radius: 12px;}
+.button1 {border-radius: 9px;}
 .button1:hover{background-color: #f7f8a3;}
 
-.button2 {border-radius: 12px;}
+.button2 {border-radius: 9px;}
 .button2:hover{background-color: #f7f8a3;}
+
+    body{
+        text-align: center;
+        text-align-last: center;
+    }
 </style>
 <button class = "button button1">Meet the Duckies</button> 
 <button class = "button button2">Drawings</button>
-
 <body>
 
 
     <h1>Letsa go babies, have a nice and cozy hatch, Keep pippin you are doing great <3</h1>
-    <p>Congrats Mr thing, Maxi and Goose!!!! You guys did it! Mommy and daddy are soooo prooooudd of ya xD</p>
-    <p>(Don't make ya mommy worry like that never again tho, or no videogames for a month)</p>
+    <p>Congrats Mr thing, Maxi, Goosea and Steven my boiii!!!! You guys did it! Mommy and daddy are soooo prooooudd of ya xD<br>(Don't make ya mommy worry like that never again tho, or no videogames for a month)</p>
     <p>RIP Wanda, we love u and were sure youll be happy in ducky heaven</p>
     <img src="ducklings.jpg">
 
