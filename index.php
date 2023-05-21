7c0510a91e8e39f5d8724de63e02dea13643628a
Pedro Lucas <plucasn@yahoo.com>
2023-05-21 00:46:21 -0300

    background color + proper text alignment

--- index.php
+++ index.php
@@ -4,22 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
     <link href="styles/style.css" rel="stylesheet" />
 </head>
 <style>
 </style>
-<button class = "button button1">Meet the Duckies</button> 
-<button class = "button button2">Drawings</button>
 <body>
-
-
     <h1>Letsa go babies, have a nice and cozy hatch, Keep pippin you are doing great <3</h1>
-    <p>Congrats Mr thing, Maxi, Goosea and Steven my boiii!!!! You guys did it! Mommy and daddy are soooo prooooudd of ya xD<br>(Don't make ya mommy worry like that never again tho, or no videogames for a month)</p>
-    <p>RIP Wanda, we love u and were sure youll be happy in ducky heaven</p>
-    <img src="ducklings.jpg">
-
+    <h2>
+    <button class = "button button1">Meet the Duckies</button> 
+    <button class = "button button2">Drawings</button>
+    </h2>
+    <p>Congrats Mr thing, Maxi, Goosea and Steven my boiii!!!!
+    <br>You guys did it! Mommy and daddy are soooo prooooudd of ya xD
+    <br>(Don't make ya mommy worry like that never again tho)
+    <br>RIP Wanda, We love u and We're sure you're happy in ducky heaven</p>
+    <img src = "ducklings.jpg" width = "600" height = "400"class = "center">
 </body>
-
 </html>
