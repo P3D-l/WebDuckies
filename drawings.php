5a9f6d39b2aec0a8a3843a1aab1d71acaff6eea9
Pedro Lucas <plucasn@yahoo.com>
2023-05-23 18:38:31 -0300

    .thml fix

--- drawings.php
+++ drawings.php
@@ -11,7 +11,7 @@
     <h2>
     <?php 
     echo '<div class = "pill-nav">
-        <a href = "index.php">Home</a>
+        <a href = "index.html">Home</a>
         <a href = "meet_the_duckies.php">Meet the Duckies</a>
         <a href = "drawings.php">Drawings</a>
     </div>'
