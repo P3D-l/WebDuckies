54c1d956ed1ca0b5a6dba60b3217f88d509eb90f
Pedro <plucasn@yahoo.com>
2023-07-01 20:01:59 -0300

    global pill nav master file

---
+++ global_pill_nav.php
@@ -0,0 +1,10 @@
+<?php
+
+echo '<div class = "pill-nav">
+
+    <a href = "index.php">Home</a>
+    <a href = "meet_the_duckies.php">Meet the Duckies</a>
+    <a href = "drawings.php">Drawings</a>
+
+</div>';
+?>
